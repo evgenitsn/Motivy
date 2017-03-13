a4c58c1774d3c62dac948ba17878d2fa1167c1ec
Evgeni Nikolov <evgeni.tsv.nikolov@gmail.com>
2017-03-13 23:42:23 +0200

    Added NativeBase and merge logic for Android and iOS

---
+++ ios/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Container, Content, Header, Title, Left, Body, Right, Button, Text } from 'native-base'
+
+class App extends Component {
+  render () {
+    <Container>
+      <Header>
+        <Left />
+        <Body>
+          <Title>Header</Title>
+        </Body>
+        <Right />
+      </Header>
+    </Container>
+  }
+}
+
+export default App
